3ae849d7f584744cad52057b9623cd9ef154f2b2
LuDiemert <lucianadiemert@gmail.com>
2024-03-05 22:31:58 -0300

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero";
+import ImageSlider from "./components/ImageSlider";
+import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
+import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
     </div>
   );
 }
 
-export default App;
+
